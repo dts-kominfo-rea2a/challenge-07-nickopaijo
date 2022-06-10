71070a64c5816b5459da9b6bb51af29ba834c14a
nickopaijo <nickopaijo@gmail.com>
2022-06-10 17:24:35 +0700

    challenge solved

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    let audienceIXX = await promiseTheaterIXX();
+    let audienceVGC = await promiseTheaterVGC();
+
+    let emotionalAudience = [...audienceIXX, ...audienceVGC];
+    let totallyEmotional = emotionalAudience.filter(
+      (emotionalResult) => emotionalResult.hasil === emotion
+    ).length;
+
+    return totallyEmotional;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
